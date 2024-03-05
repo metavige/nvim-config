24672fe4c6f8ad1a9dab1302beb0d2741a97db8a
metavige <metavige@gmail.com>
2024-03-05 22:41:31 +0800

    feat: update to lazyvim
    - change vim pack manage to lazy-vim

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,12 @@
+return {
+
+  { "patstockwell/vim-monokai-tasty" },
+
+  -- Confinure LazyVim to load vim-monokai-tasty
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vim-monokai-tasty",
+    },
+  },
+}
--- plugin/mason.lua
+++
@@ -1,8 +0,0 @@
--- Setup Mason
-require("mason").setup()
-require("mason-lspconfig").setup {
-  -- ensure_installed = { "lua_ls" },
-  -- automatically setup servers
-  automatic_installation = true
-}
-
